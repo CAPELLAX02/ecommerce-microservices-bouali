43367258851ee997a24a54e4d435c9ec36a03dc4
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-26 19:00:54 +0300

    Complete the Email Service & Implement the Notification Consumer service

--- services/notification/src/main/java/com/capellax/ecommerce/kafka/NotificationConsumer.java
+++ services/notification/src/main/java/com/capellax/ecommerce/kafka/NotificationConsumer.java
@@ -1,10 +1,12 @@
 package com.capellax.ecommerce.kafka;
 
+import com.capellax.ecommerce.email.EmailService;
 import com.capellax.ecommerce.enums.NotificationType;
 import com.capellax.ecommerce.kafka.order.OrderConfirmation;
 import com.capellax.ecommerce.kafka.payment.PaymentConfirmation;
 import com.capellax.ecommerce.model.Notification;
 import com.capellax.ecommerce.repository.NotificationRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,12 +20,12 @@ import java.time.LocalDateTime;
 public class NotificationConsumer {
 
     private final NotificationRepository notificationRepository;
-    // private final EmailService emailService
+    private final EmailService emailService;
 
     @KafkaListener(topics = "payment-topic")
     public void consumePaymentSuccessNotification(
             PaymentConfirmation paymentConfirmation
-    ) {
+    ) throws MessagingException {
         log.info("Consuming the message from 'payment-topic' Topic:: {}", paymentConfirmation);
         notificationRepository.save(
                 Notification.builder()
@@ -32,13 +34,19 @@ public class NotificationConsumer {
                         .paymentConfirmation(paymentConfirmation)
                         .build()
         );
-        // TODO: Send email
+        var customerName = paymentConfirmation.customerFirstName() + " " + paymentConfirmation.customerLastName();
+        emailService.sendPaymentSuccessfulEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+        );
     }
 
     @KafkaListener(topics = "order-topic")
     public void consumeOrderSuccessNotification(
             OrderConfirmation orderConfirmation
-    ) {
+    ) throws MessagingException {
         log.info("Consuming the message from 'order-topic' Topic:: {}", orderConfirmation);
         notificationRepository.save(
                 Notification.builder()
@@ -47,7 +55,14 @@ public class NotificationConsumer {
                         .orderConfirmation(orderConfirmation)
                         .build()
         );
-        // TODO: Send email
+        var customerName = orderConfirmation.customer().firstName() + " " + orderConfirmation.customer().lastName();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
     }
 
 }
